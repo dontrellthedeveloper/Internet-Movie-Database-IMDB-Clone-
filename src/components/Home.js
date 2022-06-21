4c6602099776f96b26bc96fc4d6fb6bcfb2091ab
Dontrell K. Washington <dontrellthedeveloper@hotmail.com>
2022-06-21 02:29:32 -0400

    added the ability to fetch data from TMDB API to home component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,6 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
+
+import {API_URL, API_KEY, IMAGE_BASE_URL, POSTER_SIZE, BACKDROP_SIZE} from "../config";
 
 //import Components
 import HeroImage from "./elements/HeroImage";
@@ -8,7 +10,15 @@ import MovieThumb from "./elements/MovieThumb";
 import LoadMoreBtn from "./elements/LoadMoreBtn";
 import Spinner from "./elements/Spinner";
 
+//Custom Hooks
+import {useHomeFetch} from "./hooks/useHomeFetch";
+
 const Home = () => {
+    const [{state, loading, error}, fetchMovies] = useHomeFetch();
+
+    console.log(state);
+
+
     return (
         <>
            <HeroImage/>
