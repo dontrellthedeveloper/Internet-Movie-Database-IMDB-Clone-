c75b797de0717cac07873b12666149438abf4a49
Dontrell K. Washington <dontrellthedeveloper@hotmail.com>
2022-06-21 21:40:30 -0400

    added hero image and props

--- src/components/Home.js
+++ src/components/Home.js
@@ -18,10 +18,17 @@ const Home = () => {
 
     console.log(state);
 
+    if (error) return <div>Something went wrong...</div>
+    if (!state.movies[0]) return <Spinner/>
+
 
     return (
         <>
-           <HeroImage/>
+           <HeroImage
+            image={`${IMAGE_BASE_URL}${BACKDROP_SIZE}${state.heroImage.backdrop_path}`}
+            title={state.heroImage.original_title}
+            text={state.heroImage.overview}
+           />
            <SearchBar/>
            <Grid/>
            <MovieThumb/>
