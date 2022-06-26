ebcbb1b6fb03872b974a713de5af07a6323c258a
Dontrell K. Washington <dontrellthedeveloper@hotmail.com>
2022-06-26 13:56:57 -0400

    added search component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,6 @@
 import React, {useState} from "react";
 
-import {API_URL, API_KEY, IMAGE_BASE_URL, POSTER_SIZE, BACKDROP_SIZE} from "../config";
+import {POPULAR_BASE_URL, SEARCH_BASE_URL, IMAGE_BASE_URL, POSTER_SIZE, BACKDROP_SIZE} from "../config";
 
 //import Components
 import HeroImage from "./elements/HeroImage";
@@ -23,9 +23,18 @@ const Home = () => {
     }, fetchMovies] = useHomeFetch();
     const [searchTerm, setSearchTerm] = useState('');
 
+
+    const searchMovies = search => {
+        const endpoint = search ? SEARCH_BASE_URL + search : POPULAR_BASE_URL;
+
+        setSearchTerm(search)
+        fetchMovies(endpoint)
+    }
+
+
     const loadMoreMovies = () => {
-        const searchEndpoint = `${API_URL}search/movie?api_key=${API_KEY}&query=${searchTerm}&page=${currentPage + 1}`;
-        const popularEndpoint = `${API_URL}movie/popular?api_key=${API_KEY}&page=${currentPage + 1}`;
+        const searchEndpoint = `${SEARCH_BASE_URL}&query=${searchTerm}&page=${currentPage + 1}`;
+        const popularEndpoint = `${POPULAR_BASE_URL}&page=${currentPage + 1}`;
 
         const endpoint = searchTerm ? searchEndpoint : popularEndpoint;
 
@@ -40,12 +49,14 @@ const Home = () => {
 
     return (
         <>
+            {!searchTerm && (
            <HeroImage
             image={`${IMAGE_BASE_URL}${BACKDROP_SIZE}${heroImage.backdrop_path}`}
             title={heroImage.original_title}
             text={heroImage.overview}
            />
-           <SearchBar/>
+                )}
+           <SearchBar callback={searchMovies}/>
            <Grid header={searchTerm ? 'Search Result' : 'Popular Movies'}>
                {movies.map(movie => (
                    <MovieThumb
@@ -53,7 +64,7 @@ const Home = () => {
                     clickable
                     image={
                        movie.poster_path
-                        ? `${IMAGE_BASE_URL}${POSTER_SIZE}${movie.poster_path}`
+                        ? IMAGE_BASE_URL + POSTER_SIZE + movie.poster_path
                         : NoImage
                    }
                     movieId={movie.id}
@@ -64,7 +75,9 @@ const Home = () => {
                }
            </Grid>
             {loading && <Spinner/>}
+            {currentPage < totalPages && !loading &&(
            <LoadMoreBtn text="Load More" callback={loadMoreMovies}/>
+                )}
         </>
     );
 };
