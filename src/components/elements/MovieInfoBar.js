ed8bbab63ce88a6288d5e8995091c94d71f05131
Dontrell K. Washington <dontrellthedeveloper@hotmail.com>
2022-06-27 14:01:51 -0400

    added movie info bar and actors to movie componentt

--- src/components/elements/MovieInfoBar.js
+++ src/components/elements/MovieInfoBar.js
@@ -1,5 +1,38 @@
-import React from 'react';
 
-const MovieInfoBar = () => <div>Movie Info Bar</div>;
+import React from 'react';
+import FontAwesome from "react-fontawesome";
+
+import {calcTime, convertMoney} from "../../helpers";
+
+import {StyledMovieInfo} from "../styles/StyledMovieInfo";
+
+const MovieInfoBar = ({time, budget, revenue}) => {
+    return (
+        <StyledMovieInfo>
+            <div className="movieinfobar-content">
+                <div className="movieinfobar-content-col">
+                    <FontAwesome className="fa-time" name="clock-o" size="2x" />
+                    <span className="movieinfobar-info">
+                        Running time: {calcTime(time)}
+                    </span>
+                </div>
+
+                <div className="movieinfobar-content-col">
+                    <FontAwesome className="fa-budget" name="money" size="2x" />
+                    <span className="movieinfobar-info">
+                        Budget: {convertMoney(budget)}
+                    </span>
+                </div>
+
+                <div className="movieinfobar-content-col">
+                    <FontAwesome className="fa-revenue" name="ticket" size="2x" />
+                    <span className="movieinfobar-info">
+                        Revenue: {convertMoney(revenue)}
+                    </span>
+                </div>
+            </div>
+        </StyledMovieInfo>
+    );
+};
 
 export default MovieInfoBar;
